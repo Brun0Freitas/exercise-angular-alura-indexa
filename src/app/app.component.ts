3fa6450d298c8f070e6cfaa7146bc5c25c0a55df
Bruno Oliveira <brunoofreitas@bb.com.br>
2024-11-05 13:05:40 -0300

    finalizado o primeiro curso: componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
+
+import agenda from './agenda.json';
+interface Icontato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent],
+  imports: [
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-alura-indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  listaContatos: Icontato[] = agenda;
+
+  filtroPorTexto: string = '';
+
+  filtrarContatoPorTexto(): Icontato[] {
+    if (!this.filtroPorTexto) {
+      return this.listaContatos;
+    }
+    return this.listaContatos.filter((contato) => {
+      return contato.nome
+        .toLowerCase()
+        .includes(this.filtroPorTexto.toLowerCase());
+    });
+  }
+
+  filtrarContatoPorLetra(letra: string): Icontato[] {
+    return this.filtrarContatoPorTexto().filter((contato) => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
+  }
 }
