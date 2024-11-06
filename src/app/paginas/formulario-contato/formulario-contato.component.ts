cff30a86acc02059513e0a9b922cf01b5e6c81e7
Bruno Oliveira <brunoofreitas@bb.com.br>
2024-11-06 10:29:31 -0300

    finalizada a validacao do form

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,26 +1,45 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-formulario-contato',
   standalone: true,
-  imports: [ContainerComponent, SeparadorComponent, ReactiveFormsModule],
+  imports: [
+    ContainerComponent,
+    SeparadorComponent,
+    ReactiveFormsModule,
+    NgClass,
+  ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css',
 })
 export class FormularioContatoComponent {
-  contatoForm: FormGroup;
+  contatoForm!: FormGroup;
 
   constructor() {
     this.contatoForm = new FormGroup({
-      nome: new FormControl('Bruno'),
-      telefone: new FormControl('(17)97070-7070'),
-      email: new FormControl('bruno@bb.com.br'),
-      aniversario: new FormControl('01/01/2021'),
-      redes: new FormControl('bruno@instagram.com'),
-      observacoes: new FormControl('Olá, mundo!'),
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl(''),
     });
+  }
+
+  salvarContato() {
+    console.log(this.contatoForm.value);
+  }
+
+  cancelar() {
+    console.log('Formulário cancelado');
   }
 }
